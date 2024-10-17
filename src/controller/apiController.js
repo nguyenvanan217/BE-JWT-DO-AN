288952e7ce28739be62bcd41de4dfa54144bc033
An Nguyen <annguyenmxh@gmail.com>
2024-10-17 11:09:38 +0700

    #11.8

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -10,30 +10,30 @@ const handleRegister = async (req, res) => {
   try {
     if (!req.body.email || !req.body.phone || !req.body.password) {
       return res.status(200).json({
-        EM: "Missing required parameters", 
+        EM: "Missing required parameters",
         EC: "1",
-        DT: "", 
+        DT: "",
       });
     }
     if (req.body.password && req.body.password.length < 4) {
       return res.status(200).json({
-        EM: "Your password must have more than 3 letters", 
+        EM: "Your password must have more than 3 letters",
         EC: "1",
-        DT: "", 
+        DT: "",
       });
     }
     //service: create user
     let data = await loginRegisterService.registerNewUser(req.body);
     return res.status(200).json({
-      EM: data.EM, 
+      EM: data.EM,
       EC: data.EC,
-      DT: "", 
+      DT: "",
     });
   } catch (error) {
     return res.status(500).json({
-      EM: "error from server", 
+      EM: "error from server",
       EC: "-1",
-      DT: "", 
+      DT: "",
     });
   }
 };
@@ -42,17 +42,19 @@ const handleLogin = async (req, res) => {
     // console.log('>>>>req', req.body);
     let data = await loginRegisterService.handleUserLogin(req.body);
     //set cookie
-    res.cookie("jwt", data.DT.access_token, {httpOnly: true, maxAge: 60 * 60 * 1000}) 
+    if (data && data.DT && data.DT.access_token) {
+      res.cookie("jwt", data.DT.access_token, {httpOnly: true, maxAge: 60 * 60 * 1000});
+    }
     return res.status(200).json({
-      EM: data.EM, 
+      EM: data.EM,
       EC: data.EC,
-      DT: data.DT, 
+      DT: data.DT,
     });
   } catch (error) {
     return res.status(500).json({
-      EM: "error from server", 
+      EM: "error from server",
       EC: "-1",
-      DT: "", 
+      DT: "",
     });
   }
 };
