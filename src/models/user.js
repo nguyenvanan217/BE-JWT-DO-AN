07a1b0739afacf868160d9764072735864f77c07
An Nguyen <annguyen828721665@gmail.com>
2024-11-23 18:36:28 +0700

    push all

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       User.belongsTo(models.Group);
       User.belongsToMany(models.Project, { through: "Project_User" });
-    }
+    } 
   }
   User.init(
     {
